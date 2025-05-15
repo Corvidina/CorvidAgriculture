f4cb8ed59daf8d82148f54c82b90c1346677f07a
Corvidina <corvidina@gmail.com>
2025-05-15 19:08:37 -0400

    Added GUI Class; Implemented Inventory Framework API.

--- src/main/java/com/corvidina/corvidAgriculture/gui/AgricultureMenu.java
+++ src/main/java/com/corvidina/corvidAgriculture/gui/AgricultureMenu.java
@@ -1,4 +1,18 @@
 package com.corvidina.corvidAgriculture.gui;
 
-public class AgricultureMenu {
+import me.devnatan.inventoryframework.View;
+
+public class AgricultureMenu extends View {
+
+    public AgricultureMenu(){
+
+    }
+    @Override
+    public void onInit(){
+
+    }
+
+    public void open(){
+
+    }
 }
